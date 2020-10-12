db82a62134eb7494f13c273121c5c716c34d8bfa
psyKomicron <julien@cavaweb.net>
2020-10-12 13:11:44 +0200

    config changes

--- Multitool/Properties/AssemblyInfo.cs
+++ Multitool/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("YoutubeMP3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MultiTool")]
+[assembly: AssemblyDescription("A swedish knive look-a-like for lazy people like me")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("YoutubeMP3")]
+[assembly: AssemblyCompany("psyKomicron")]
+[assembly: AssemblyProduct("MultiTool")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
