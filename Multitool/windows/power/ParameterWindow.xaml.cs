acb26b15e385b9255b3e8de3e667967f9aad1d6b
Julien Marquet <49617466+psyKomicron@users.noreply.github.com>
2020-10-16 14:03:00 +0200

    creating global window to show a sub-app parameters

---
+++ Multitool/windows/power/ParameterWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MultiTool.windows.power
+{
+    /// <summary>
+    /// Interaction logic for ParameterWindow.xaml
+    /// </summary>
+    public partial class ParameterWindow : Window
+    {
+        public ParameterWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
