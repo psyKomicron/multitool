cadc039057e6e2e2cd276d030ad1d081f5b8a280
psyKomicron <julien@cavaweb.net>
2020-10-22 00:58:43 +0200

    save user preferences on exit

--- Multitool/App.xaml.cs
+++ Multitool/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.PreferencesManager;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,15 @@ namespace MultiTool
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            PreferenceManager manager = Tool.GetPreferenceManager();
+            manager.SavePreferences();
+
+            stopwatch.Stop();
+            Console.WriteLine("Preference file creation/writing time : " + stopwatch.ElapsedMilliseconds);
+        }
     }
 }
