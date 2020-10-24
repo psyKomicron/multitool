4db0105e5779042fcb4d2d2ae5543150d9aa8865
psyKomicron <julien@cavaweb.net>
2020-10-24 04:05:53 +0200

    version update

--- Multitool/Properties/AssemblyInfo.cs
+++ Multitool/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
 [assembly: NeutralResourcesLanguage("en")]
