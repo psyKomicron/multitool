f6cad6e6161cb2f94b9a74670f29004a3edf0b4d
psyKomicron <julien@cavaweb.net>
2020-10-25 02:50:02 +0100

    code optimisation & added functionnalities to deserialize from preference file

--- BusinessLayer/PreferencesManager/PreferenceManager.cs
+++ BusinessLayer/PreferencesManager/PreferenceManager.cs
@@ -3,11 +3,7 @@ using BusinessLayer.Parsers.Errors;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net.Mail;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLayer.PreferencesManager
 {
@@ -15,61 +11,49 @@ namespace BusinessLayer.PreferencesManager
     {
         private List<WindowPreferenceManager> childs = new List<WindowPreferenceManager>(5);
 
-        public string Path { get; private set; }
+        public string Path { get; set; }
 
-        public PreferenceManager()
-        {
-            Path = @"C:\Users\julie\Documents\MultiTool\preferences\userpreferences.json";
-        }
-
-        public PreferenceManager(string path)
+        public PreferenceManager(string path = @"C:\Users\julie\Documents\MultiTool\preferences\userpreferences.json")
         {
             Path = path;
         }
 
         /// <summary>
-        /// Serialize the childs.
+        /// Serialize the childs to a JSON file.
         /// </summary>
-        /// <param name="path"></param>
         public void SavePreferences()
         {
-            PreferenceManager data = ParsePreferences(Path);
+            string path = Path; // to keep the same path even if the property is changed
+            PreferenceManager data = ParsePreferences(path);
 
             if (data == null)
             {
-                WritePreferences(CreateData(), Path);
+                WritePreferences(CreateData(), path);
             }
             else
             {
-                bool noWrite = true;
+                bool equivalent = true;
                 // compare and write to optimize performance
-                for (int i = 0; i < data.childs.Count && i < childs.Count && noWrite; i++)
+                for (int i = 0; i < data.childs.Count && i < childs.Count && equivalent; i++)
                 {
-                    noWrite = !data.childs[i].IsEquivalentTo(childs[i]);
+                    equivalent = data.childs[i].IsEquivalentTo(childs[i]);
                 }
-                if (noWrite)
+                if (!equivalent)
                 {
-                    WritePreferences(CreateData(), Path);
+                    WritePreferences(CreateData(), path);
+                }
+                else
+                {
+                    Console.WriteLine("\tCurrent preferences are the same as the saved ones, no writing required");
                 }
             }
         }
 
-        private string CreateData()
-        {
-            StringBuilder data = new StringBuilder();
-            data.Append("{\"preferences\":[");
-            for (int i = 0; i < childs.Count; i++)
-            {
-                data.Append(childs[i].Flatten()).Append("}");
-                if (i != childs.Count - 1)
-                {
-                    data.Append(",");
-                }
-            }
-            data.Append("]}");
-            return data.ToString();
-        }
-
+        /// <summary>
+        /// Add a <see cref="WindowPreferenceManager"/> to the list.
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <returns></returns>
         public WindowPreferenceManager AddPreferenceManager(WindowPreferenceManager manager)
         {
             if (childs.Contains(manager)) // WPM implements IQuatable
@@ -95,29 +79,29 @@ namespace BusinessLayer.PreferencesManager
             return manager;
         }
 
-        internal void AddPreferenceManagers(List<WindowPreferenceManager> managers)
+        public WindowPreferenceManager GetWindowManager(string name)
         {
-            childs = managers;
+            foreach (WindowPreferenceManager manager in childs)
+            {
+                if (manager.ItemName.Equals(name))
+                {
+                    return manager;
+                }
+            }
+            return null;
         }
 
-        private PreferenceManager ParsePreferences(string path)
+        public void DeserializePreferenceManager()
+        {
+            PreferenceManager manager = ParsePreferences(Path);
+            childs = manager.childs;
+        }
+
+        internal PreferenceManager ParsePreferences(string path)
         {
             try
             {
-                byte[] bytes;
-                using (FileStream stream = new FileStream(path, FileMode.Open))
-                {
-                    bytes = new byte[stream.Length];
-                    int bytesToRead = (int)stream.Length;
-                    int bytesRead = 0;
-                    int n = 0;
-                    while ((n = stream.Read(bytes, bytesRead, bytesToRead)) != 0)
-                    {
-                        bytesRead += n;
-                        bytesToRead -= n;
-                    }
-                }
-                string rawData = Encoding.UTF8.GetString(bytes);
+                string rawData = ReadPreferenceFile(path);
                 string data = string.Empty;
                 bool inString = false;
 
@@ -125,14 +109,7 @@ namespace BusinessLayer.PreferencesManager
                 {
                     if (rawData[i] == '"')
                     {
-                        if (inString)
-                        {
-                            inString = false;
-                        }
-                        else
-                        {
-                            inString = true;
-                        }
+                        inString = !inString;
                     }
 
                     if (IsControlChar(rawData[i]))
@@ -147,27 +124,58 @@ namespace BusinessLayer.PreferencesManager
                         data += rawData[i];
                     }
                 }
-                Console.WriteLine(data);
-                PreferenceManager manager = null;
-                try
-                {
-                    manager = JsonParser.ParsePreferenceManager(data);
-                }
-                catch (JsonFormatException jfe)
-                {
-                    Console.Error.WriteLine(jfe.Message);
-                }
 
-                return manager;
+                return JsonParser.ParsePreferenceManager(data);
             }
-            catch (FileNotFoundException) 
+            catch (FileNotFoundException)
             {
                 Console.Error.WriteLine("Not able to find \"" + path + "\"");
+            }
+            catch (JsonFormatException jfe)
+            {
+                Console.Error.WriteLine(jfe.Message);
             }
             return null;
         }
 
-        private bool IsControlChar(char c) => c == '\r' || c == '\n' || c == '\t' || c == ' ';
+        internal void AddPreferenceManagers(List<WindowPreferenceManager> managers)
+        {
+            childs = managers;
+        }
+
+        private string CreateData()
+        {
+            StringBuilder data = new StringBuilder();
+            data.Append("{\"preferences\":[");
+            for (int i = 0; i < childs.Count; i++)
+            {
+                data.Append(childs[i].Flatten()).Append("}");
+                if (i != childs.Count - 1)
+                {
+                    data.Append(",");
+                }
+            }
+            data.Append("]}");
+            return data.ToString();
+        }
+
+        private string ReadPreferenceFile(string path)
+        {
+            byte[] bytes;
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                bytes = new byte[stream.Length];
+                int bytesToRead = (int)stream.Length;
+                int bytesRead = 0;
+                int n = 0;
+                while ((n = stream.Read(bytes, bytesRead, bytesToRead)) != 0)
+                {
+                    bytesRead += n;
+                    bytesToRead -= n;
+                }
+            }
+            return Encoding.UTF8.GetString(bytes);
+        }
 
         private void WritePreferences(string data, string path)
         {
@@ -184,5 +192,8 @@ namespace BusinessLayer.PreferencesManager
                 Console.Error.WriteLine(e.ToString());
             }
         }
+
+        private bool IsControlChar(char c) => c == '\r' || c == '\n' || c == '\t' || c == ' ';
+
     }
 }
