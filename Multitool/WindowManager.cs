e22a32663a66f3e214fe2df96bc4a03ec8448381
psyKomicron <julien@cavaweb.net>
2020-11-15 18:18:17 +0100

    created to manage window (creation and opening) as MainWindow did it

---
+++ Multitool/WindowManager.cs
@@ -0,0 +1,77 @@
+﻿using MultiTool.Windows;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace MultiTool
+{
+    public static class WindowManager
+    {
+        private static List<Window> windows = new List<Window>(3);
+        
+        /// <summary>
+        /// Opens a window of the <typeparamref name="WindowType"/> type if a window of <typeparamref name="WindowType"/>
+        /// is not already openened.
+        /// </summary>
+        /// <typeparam name="WindowType">Type of the window to open</typeparam>
+        /// <returns>Returns true if the <see cref="Window"/> was not open and thus has been created, false if the 
+        /// <see cref="Window"/> was already opened and thus was not created.</returns>
+        public static bool Open<WindowType>() where WindowType : Window, new()
+        {
+            if (windows.Count > 0)
+            {
+                Window instance = windows.Find((w) => w is WindowType);
+                if (instance != null)
+                {
+                    instance.Activate();
+                    return false;
+                }
+                else
+                {
+                    CreateAndOpen<WindowType>();
+                    return true;
+                }
+            }
+            else
+            {
+                CreateAndOpen<WindowType>();
+                return true;
+            }
+        }
+
+        private static void CreateAndOpen<WindowType>() where WindowType : Window, new()
+        {
+            Window w = new WindowType();
+            w.Closing += Window_Closing;
+            w.Closed += ChildWindow_Closed;
+
+            w.Show();
+            windows.Add(w);
+        }
+
+
+        #region events
+
+        private static void ChildWindow_Closed(object sender, EventArgs e)
+        {
+            if (sender is Window window)
+            {
+                Window instanceWindow = windows.Find((w) => w.Name == window.Name);
+                if (instanceWindow != null)
+                {
+                    windows.Remove(instanceWindow);
+                }
+            }
+        }
+
+        private static void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (sender is ISerializableWindow serializableWindow)
+            {
+                serializableWindow.Serialize();
+            }
+        }
+
+        #endregion
+    }
+}
