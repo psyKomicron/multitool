002b805acff3ab33cdee59b72f6ef6e0c1011c83
psyKomicron <julien@cavaweb.net>
2020-11-15 18:20:33 +0100

    changed directory

---
+++ BusinessLayer/PreferencesManager/IPreferenceManager.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessLayer.PreferencesManager
+{
+    public interface IPreferenceManager<WindowManagerType> where WindowManagerType : IWindowPreferenceManager
+    {
+        string Path { get; set; }
+
+        void SerializePreferenceManager();
+        void DeserializePreferenceManager();
+        WindowManagerType GetWindowManager(string name);
+        WindowManagerType AddPreferenceManager(WindowManagerType manager);
+    }
+}
---
+++ BusinessLayer/PreferencesManager/IWindowPreferenceManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLayer.PreferencesManager
+{
+    public interface IWindowPreferenceManager
+    {
+        string ItemName { get; set; }
+        Dictionary<string, string> Values { get; set; }
+
+        bool IsEquivalentTo(Dictionary<string, string> data);
+        bool IsEquivalentTo(IWindowPreferenceManager manager);
+        bool Equals(JsonWindowPreferenceManager other);
+    }
+}
