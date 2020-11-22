434e45567871e8021ba0ddaa17b1edb93b25a574
psyKomicron <julien@cavaweb.net>
2020-11-22 03:16:02 +0100

    Create IPropertyLoader.cs

---
+++ BusinessLayer/Reflection/IPropertyLoader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLayer.Reflection
+{
+    public interface IPropertyLoader
+    {
+        DtoType Load<DtoType>(Dictionary<string, string> dictionnary) where DtoType : class, new();
+    }
+}
