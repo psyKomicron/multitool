13733e61929724484e50e2b2f6ab9c00311afe4d
psyKomicron <julien@cavaweb.net>
2020-11-22 03:13:29 +0100

    Added documentation for SerializePreferenceManager()

--- BusinessLayer/PreferencesManager/IPreferenceManager.cs
+++ BusinessLayer/PreferencesManager/IPreferenceManager.cs
@@ -1,9 +1,13 @@
-﻿namespace BusinessLayer.PreferencesManager
+﻿namespace BusinessLayer.PreferencesManagers
 {
     public interface IPreferenceManager<WindowManagerType> where WindowManagerType : IWindowPreferenceManager
     {
         string Path { get; set; }
 
+        /// <summary>
+        /// Save the informations stored in the <see cref="IPreferenceManager{WindowManagerType}"/>
+        /// to a file (the file format is determined by implementation).
+        /// </summary>
         void SerializePreferenceManager();
         void DeserializePreferenceManager();
         WindowManagerType GetWindowManager(string name);
