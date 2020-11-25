52df32f039f2d18ed0f9564b253f36f5b7bf622d
psyKomicron <julien@cavaweb.net>
2020-11-25 02:22:15 +0100

    Moved ./Reflection -> ./Reflection/ObjectFlatteners

--- BusinessLayer/Reflection/ObjectFlatteners/BasicObjectFlattener.cs
+++ BusinessLayer/Reflection/ObjectFlatteners/BasicObjectFlattener.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace BusinessLayer.Reflection
+namespace BusinessLayer.Reflection.ObjectFlatteners
 {
-    public class BasicObjectFlattener : IObjectFlattener
+    public class BasicObjectFlattener : IObjectFlattener<Dictionary<string, string>>
     {
         public Dictionary<string, string> Flatten<T>(T o) where T : class
         {
             Dictionary<string, string> flatProperties = new Dictionary<string, string>();
             PropertyInfo[] properties = GetPropertyInfos<T>();
+
             for (int i = 0; i < properties.Length; i++)
             {
                 if (properties[i].MemberType == MemberTypes.Property)
@@ -25,19 +23,20 @@ namespace BusinessLayer.Reflection
                         {
                             flatProperties.Add(key, properties[i].GetValue(o).ToString());
                         }
-                        else // this does NOT work
+                        else 
                         {
                             Dictionary<string, string> props = Flatten(o);
                             StringBuilder stringBuilder = new StringBuilder();
                             foreach (var prop in props)
                             {
-                                stringBuilder.Append("[" + prop.Key).Append(":").Append("]");
+                                stringBuilder.Append("[" + prop.Key).Append(":").Append(prop.Value).Append("]");
                             }
                             flatProperties.Add(key, stringBuilder.ToString());
                         }
                     }
                 }
             }
+
             return flatProperties;
         }
 
---
+++ BusinessLayer/Reflection/ObjectFlatteners/IObjectFlattener.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLayer.Reflection.ObjectFlatteners
+{
+    public interface IObjectFlattener<FlatType>
+    {
+        FlatType Flatten<T>(T o) where T : class;
+    }
+}
