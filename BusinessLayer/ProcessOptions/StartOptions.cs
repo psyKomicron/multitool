74687c9bd0e80cc7fbefd40a0ff0e5cab80cace8
psyKomicron <julien@cavaweb.net>
2020-11-25 02:20:58 +0100

    Update StartOptions.cs

--- BusinessLayer/ProcessOptions/StartOptions.cs
+++ BusinessLayer/ProcessOptions/StartOptions.cs
@@ -1,15 +1,12 @@
 ﻿using BusinessLayer.ProcessOptions.EnumTranslaters;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLayer.ProcessOptions
 {
     public class StartOptions<EnumType> where EnumType : Enum
     {
         public List<EnumType> Options { get; set; }
-        public EnumTranslater<EnumType> Translater { get; set; }
+        public IEnumTranslater<EnumType> Translater { get; set; }
     }
 }
