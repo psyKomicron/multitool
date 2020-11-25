641255bce1be7268b9526f90dfd0e70fb9b4e265
psyKomicron <julien@cavaweb.net>
2020-11-25 02:16:58 +0100

    Removed unecessary interface generic type

--- BusinessLayer/PreferencesManager/IPreferenceManager.cs
+++ BusinessLayer/PreferencesManager/IPreferenceManager.cs
@@ -1,16 +1,16 @@
 ﻿namespace BusinessLayer.PreferencesManagers
 {
-    public interface IPreferenceManager<WindowManagerType> where WindowManagerType : IWindowPreferenceManager
+    public interface IPreferenceManager
     {
         string Path { get; set; }
 
+        void AddWindowManager(WindowPreferenceManager manager);
+        void DeserializePreferenceManager();
+        DataType GetWindowManager<DataType>(string name) where DataType : class, new();
         /// <summary>
         /// Save the informations stored in the <see cref="IPreferenceManager{WindowManagerType}"/>
         /// to a file (the file format is determined by implementation).
         /// </summary>
         void SerializePreferenceManager();
-        void DeserializePreferenceManager();
-        WindowManagerType GetWindowManager(string name);
-        WindowManagerType AddPreferenceManager(WindowManagerType manager);
     }
 }
