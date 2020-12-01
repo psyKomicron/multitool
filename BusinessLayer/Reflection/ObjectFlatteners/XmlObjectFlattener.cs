452c362d52f2d0d297750f1fcc4c43d86ab723ee
psyKomicron <julien@cavaweb.net>
2020-12-02 00:54:19 +0100

    Corrected improper behavior when the object was "complex"

--- BusinessLayer/Reflection/ObjectFlatteners/XmlObjectFlattener.cs
+++ BusinessLayer/Reflection/ObjectFlatteners/XmlObjectFlattener.cs
@@ -1,36 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace BusinessLayer.Reflection.ObjectFlatteners
 {
-    public class XmlObjectFlattener : IObjectFlattener<XmlNode>
+    public class XmlObjectFlattener : ObjectFlattener<XmlNode>
     {
-        public XmlNode Flatten<T>(T o) where T : class
+        /// <summary>
+        /// Transforms a object/class to xml. The properties are under the "properties" node.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public override XmlNode Flatten<T>(T o)
         {
-            XmlDocument xml = new XmlDocument();
-            XmlNode root = xml.CreateElement("properties");
+            return RecursiveFlatten(o, new XmlDocument());
+        }
 
-            PropertyInfo[] propertyInfos = GetPropertyInfos<T>();
+        private XmlNode RecursiveFlatten<T>(T o, XmlDocument xml , string rootName = "properties", int depth = 0)
+        {
+            // check to prevent stack overflow
+            if (depth > 100)
+            {
+                XmlNode xml1 = xml.CreateElement(rootName);
+                xml1.InnerText = o.ToString();
+                return xml1;
+            }
+
+            XmlNode root = xml.CreateElement(rootName);
+
+            PropertyInfo[] propertyInfos = o.GetType().GetProperties();
 
             for (int i = 0; i < propertyInfos.Length; i++)
             {
                 if (propertyInfos[i].MemberType == MemberTypes.Property)
                 {
+                    object value = propertyInfos[i].GetValue(o);
+                    string name = propertyInfos[i].Name;
 
+                    if (value != null)
+                    {
+                        if (IsPrimitive(value.GetType()))
+                        {
+                            XmlNode prop = xml.CreateElement(name);
+                            prop.InnerText = value.ToString();
+                            root.AppendChild(prop);
+                        }
+                        else if (IsEnumerable(propertyInfos[i].PropertyType))
+                        {
+                            IEnumerable array = propertyInfos[i].GetMethod.Invoke(o, null) as IEnumerable;
+                            Type[] genericArgs = propertyInfos[i].PropertyType.GetGenericArguments();
+                            Type itemType = null;
+                            if (genericArgs.Length == 1)
+                            {
+                                itemType = genericArgs[0];
+                            }
+
+                            XmlNode arrayRoot = xml.CreateElement(name);
+                            int index = 0;
+                            foreach (var obj in array)
+                            {
+                                XmlNode xmlNode = xml.CreateElement(index.ToString());
+                                if (itemType != null)
+                                {
+                                    var item = Convert.ChangeType(obj, itemType);
+                                    
+                                    if (IsPrimitive(item.GetType()))
+                                    {
+                                        xmlNode.InnerText = item.ToString();
+                                    }
+                                    else
+                                    {
+                                        XmlNode node = RecursiveFlatten(item, xml, item.GetType().Name, depth);
+                                        xmlNode.AppendChild(node);
+                                    }
+                                    arrayRoot.AppendChild(xmlNode);
+                                }
+                                else
+                                {
+                                    xmlNode.InnerText = obj.ToString();
+                                }
+                                // update index for node names
+                                index++;
+                            }
+
+                            root.AppendChild(arrayRoot);
+                        }
+                        else
+                        {
+                            XmlNode node = RecursiveFlatten(value, xml , name, depth + 1); // recursively flatten the properties of the object
+                            root.AppendChild(node);
+                        }
+                    }
+                    else
+                    {
+                        XmlNode node = RecursiveFlatten(value, xml, name, depth + 1); // recursively flatten the properties of the object
+                        root.AppendChild(node);
+                    }
                 }
             }
 
             return root;
         }
 
-        private PropertyInfo[] GetPropertyInfos<T>()
-        {
-            return typeof(T).GetProperties();
-        }
     }
 }
