3376270a0c3042494be3ac3f8f955aacb7f0c6d8
psyKomicron <julien@cavaweb.net>
2020-12-02 00:57:22 +0100

    Changed class modifier to public

--- Multitool/DTO/UrlHistoryViewModel.cs
+++ Multitool/DTO/UrlHistoryViewModel.cs
@@ -7,7 +7,7 @@ using System.Windows.Media;
 
 namespace MultiTool.DTO
 {
-    internal class UrlHistoryViewModel
+    public class UrlHistoryViewModel
     {
         public DateTime Date { get; set; }
         public string Link { get; set; }
