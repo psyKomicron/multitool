9bab12142407aa1309ef5f9b559cd8e54392e886
psyKomicron <julien@cavaweb.net>
2020-12-15 15:21:10 +0100

    controller to start default browser on a link

---
+++ BusinessLayer/Controllers/DefaultBrowserController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.ProcessOptions.Enums;
+using System;
+using System.Diagnostics;
+
+namespace BusinessLayer.Controllers
+{
+    public class DefaultBrowserController : Controller<DefaultOptions>
+    {
+        public Uri Uri { get; set; }
+
+        public override void Execute()
+        {
+            Process.Start(Uri.AbsoluteUri);
+        }
+    }
+}
