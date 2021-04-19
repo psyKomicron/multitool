8a00bcd7e7905fea3f5b04e2bb6bdd9439f35430
psyKomicron <julien@cavaweb.net>
2021-04-19 03:45:52 +0200

    renamed AddDelegate -> CollectionAddDelegate

---
+++ BusinessLayer/FileSystem/AddDelegate.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLayer.FileSystem
+{
+    public delegate void AddDelegate<ItemType>(IList<ItemType> items, IFileSystemEntry item) where ItemType : IFileSystemEntry;
+}
