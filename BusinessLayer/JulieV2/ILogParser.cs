2556ceea172d9eee1eaec556236b00a5e86345bf
psyKomicron <julien@cavaweb.net>
2021-04-19 04:01:45 +0200

    upcoming feature to parse .json logs generated by the JulieV2 discord bot

---
+++ BusinessLayer/JulieV2/ILogParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace BusinessLayer.JulieV2
+{
+    public interface ILogParser
+    {
+        string LocateLogFile();
+        IList<BotCommand> ParseLogs(string directory);
+    }
+}
---
+++ BusinessLayer/JulieV2/User.cs
@@ -0,0 +1,9 @@
+﻿namespace BusinessLayer.JulieV2
+{
+    public class User
+    {
+        public uint Discriminator { get; internal set; }
+        public string Name { get; internal set; }
+        public string Tag { get; internal set; }
+    }
+}
