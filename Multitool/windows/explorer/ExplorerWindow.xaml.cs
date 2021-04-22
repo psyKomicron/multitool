12be35450510cf4788dbf4f9ee4058f4c85129ce
Julien Marquet <49617466+psyKomicron@users.noreply.github.com>
2021-04-22 14:32:14 +0200

    fixed previous and next behavior

--- Multitool/windows/explorer/ExplorerWindow.xaml.cs
+++ Multitool/windows/explorer/ExplorerWindow.xaml.cs
@@ -1,8 +1,8 @@
-﻿using MultiToolBusinessLayer;
+﻿using MultiTool.Tools;
+using MultiTool.ViewModels;
+using MultiToolBusinessLayer;
 using MultiToolBusinessLayer.FileSystem;
 using MultiToolBusinessLayer.Parsers;
-using MultiTool.Tools;
-using MultiTool.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -25,9 +25,9 @@ namespace MultiTool.Windows
     public partial class ExplorerWindow : Window, ISerializableWindow, INotifyPropertyChanged
     {
         private string _currentPath;
-        private string lastPath;
-        private readonly UriCleaner cleaner = new UriCleaner();
-        private Stack<string> pathHistory = new Stack<string>();
+        private UriCleaner cleaner = new UriCleaner();
+        private Stack<string> pathHistory = new Stack<string>(10);
+        private Stack<string> nextPathStack = new Stack<string>(10);
         private FileSystemManager fileSystemManager;
         private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         private Stopwatch eventStopwatch = new Stopwatch();
@@ -83,7 +83,13 @@ namespace MultiTool.Windows
 
         private async Task DisplayFiles(string path)
         {
-            ClearDisplays();
+            #region clear displays
+            CurrentFiles.Clear();
+            PathInput.Text = string.Empty;
+            PathInput.IsReadOnly = true;
+            ProgressError_TextBox.Text = string.Empty;
+            #endregion
+
             fileSystemManager.Notify = true;
 
             string[] drives = Directory.GetLogicalDrives();
@@ -95,15 +101,16 @@ namespace MultiTool.Windows
                 }
             }
 
+            #region renew cancellation token
             if (cancellationTokenSource != null)
             {
                 cancellationTokenSource.Cancel();
                 cancellationTokenSource.Dispose();
             }
             cancellationTokenSource = new CancellationTokenSource();
+            #endregion
 
             DisplayProgressBar.IsIndeterminate = true;
-            lastPath = CurrentPath;
             CurrentPath = path;
 
             await Task.Run(() => GetFiles(path, cancellationTokenSource), cancellationTokenSource.Token);
@@ -122,21 +129,20 @@ namespace MultiTool.Windows
             Application.Current.Dispatcher.Invoke(() => SortList());
         }
 
-        /// <summary>
-        /// Loads the previously visited directory.
-        /// </summary>
         private void Next()
         {
-            
+            if (nextPathStack.Count > 0)
+            {
+                pathHistory.Push(CurrentPath);
+                _ = DisplayFiles(nextPathStack.Pop());
+            }
         }
 
-        /// <summary>
-        /// Loads the current directory's parent.
-        /// </summary>
         private void Back()
         {
             if (pathHistory.Count > 0)
             {
+                nextPathStack.Push(CurrentPath);
                 _ = DisplayFiles(pathHistory.Pop());
             }
         }
@@ -152,16 +158,6 @@ namespace MultiTool.Windows
             {
                 CurrentFiles.Add(pathItems[i]);
             }
-        }
-
-        private void ClearDisplays()
-        {
-            CurrentFiles.Clear();
-
-            PathInput.Text = string.Empty;
-            PathInput.IsReadOnly = true;
-
-            ProgressError_TextBox.Text = string.Empty;
         }
 
         private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
@@ -180,9 +176,9 @@ namespace MultiTool.Windows
             });
         }
 
-        #region Events handlers
+        #region events handlers
 
-        #region calling asynchronous
+        #region window
         private void FolderHistory_Click(object sender, RoutedEventArgs e)
         {
             object folderName = (sender as Button)?.Content;
@@ -226,40 +222,14 @@ namespace MultiTool.Windows
 
         private void Previous_Click(object sender, RoutedEventArgs e)
         {
-            Back();
             e.Handled = true;
+            Back();
         }
 
         private void Next_Click(object sender, RoutedEventArgs e)
         {
-            Next();
             e.Handled = true;
-        }
-        #endregion
-
-        private void FileSystemManager_Progress(object sender, string message)
-        {
-            if (sender == null)
-            {
-                Application.Current.Dispatcher.Invoke(() => ProgressError_TextBox.Text = message);
-                return;
-            }
-
-            if (eventStopwatch.ElapsedMilliseconds > 50) //ms interval between each notification
-            {
-                Application.Current.Dispatcher.Invoke(() => CurrentPath = message);
-
-                eventStopwatch.Restart();
-            }
-        }
-
-        private void FileSystemManager_Exception(object sender, Exception exception)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                ProgressError_TextBox.Text = exception.Message;
-            });
-            Console.WriteLine(exception.Message);
+            Next();
         }
 
         private void PathInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -290,6 +260,39 @@ namespace MultiTool.Windows
             }
         }
 
+        private void HistoryListViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            Data.History.Clear();
+        }
+        #endregion
+
+        #region file system manager
+        private void FileSystemManager_Progress(object sender, string message)
+        {
+            if (sender == null)
+            {
+                Application.Current.Dispatcher.Invoke(() => ProgressError_TextBox.Text = message);
+            }
+            else if (eventStopwatch.ElapsedMilliseconds > 50) //ms interval between each notification
+            {
+                Application.Current.Dispatcher.Invoke(() => CurrentPath = message);
+
+                eventStopwatch.Restart();
+            }
+        }
+
+        private void FileSystemManager_Exception(object sender, Exception exception)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ProgressError_TextBox.Text = exception.Message;
+            });
+            Console.WriteLine(exception.Message);
+        }
+        #endregion
+
+        #region window chrome
         private void MultiToolWindowChrome_CloseClick(object sender, RoutedEventArgs e)
         {
             e.Handled = true;
@@ -302,6 +305,19 @@ namespace MultiTool.Windows
             WindowState = WindowState.Minimized;
         }
 
+        private void MultiToolWindowChrome_MaximizeClick(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            e.Handled = true;
+        }
+
         private void MultiToolWindowChrome_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -309,12 +325,8 @@ namespace MultiTool.Windows
                 DragMove();
             }
         }
+        #endregion
 
-        private void HistoryListViewMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            e.Handled = true;
-            Data.History.Clear();
-        }
         #endregion
     }
 }
