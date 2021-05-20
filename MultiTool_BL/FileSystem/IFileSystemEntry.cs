4d085f8e28a7211a5773c43d9e157551a96629c7
psyKomicron <julien@cavaweb.net>
2021-05-20 22:31:42 +0200

    added Info property to get the internal FileSystemInfo object associated & doc

--- MultiTool_BL/FileSystem/IFileSystemEntry.cs
+++ MultiTool_BL/FileSystem/IFileSystemEntry.cs
@@ -6,6 +6,7 @@ namespace Multitool.FileSystem
 {
     public interface IFileSystemEntry : IComparable, INotifyPropertyChanged, IEquatable<IFileSystemEntry>, IComparable<IFileSystemEntry>
     {
+        FileSystemInfo Info { get; }
         string Path { get; }
         long Size { get; }
         string Name { get; }
@@ -17,5 +18,28 @@ namespace Multitool.FileSystem
         bool IsCompressed { get; }
         bool IsDevice { get; }
         bool IsDirectory { get; }
+
+        /// <summary>
+        /// Deletes the file
+        /// </summary>
+        void Delete();
+
+        /// <summary>
+        /// Rename the file
+        /// </summary>
+        /// <param name="newName">The new name of the file</param>
+        void Rename(string newName);
+
+        /// <summary>
+        /// Move the file to a new directory
+        /// </summary>
+        /// <param name="newPath">The path to move the file to</param>
+        void Move(string newPath);
+
+        /// <summary>
+        /// Copy the file to a new directory
+        /// </summary>
+        /// <param name="newPath">The path to copy the file to</param>
+        void CopyTo(string newPath);
     }
 }
