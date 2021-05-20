f8a0f1d475acf800ac0a3f5542b6dd5a7da888c4
psyKomicron <julien@cavaweb.net>
2021-05-20 22:10:40 +0200

    tests

---
+++ MultiTool_Test/FileSystem/Completion/PathCompletorTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Multitool.FileSystem.Completion.Tests
+{
+    [TestClass()]
+    public class PathCompletorTests
+    {
+        [TestMethod()]
+        public void PathCompletorTest()
+        {
+            DriveInfo[] driveInfos = DriveInfo.GetDrives();
+            PathCompletor[] pathCompletors = new PathCompletor[driveInfos.Length];
+            for (int i = 0; i < driveInfos.Length; i++)
+            {
+                pathCompletors[i] = new PathCompletor();
+            }
+        }
+
+        [TestMethod()]
+        public void CompleteTest()
+        {
+            PathCompletor pathCompletor = new();
+            List<string> completedPaths = new();
+            pathCompletor.Complete(@"E:\julie\Videos\Desktop\P\ev", completedPaths);
+            Assert.IsTrue(completedPaths.Count > 0);
+        }
+    }
+}
---
+++ MultiTool_Test/Optimisation/ObjectPoolTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Multitool.Optimisation;
+
+using MultiTool_Test;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multitool.Optimisation.Tests
+{
+    [TestClass()]
+    public class ObjectPoolTests
+    {
+        [TestMethod()]
+        public void ObjectPoolTest()
+        {
+            var o = new ObjectPool<TestClass>();
+        }
+
+        [TestMethod()]
+        public void ObjectPoolTest1()
+        {
+            var o = new ObjectPool<TestClass>(10000);
+        }
+
+        [TestMethod()]
+        public void ObjectPoolTest2()
+        {
+            var o = new ObjectPool<TestClass>(Array.Empty<object>());
+            Assert.ThrowsException<ArgumentException>(() => new ObjectPool<TestClass>(true));
+        }
+
+        [TestMethod()]
+        public void ObjectPoolTest3()
+        {
+            var o = new ObjectPool<TestClass>(10, Array.Empty<object>());
+            Assert.ThrowsException<ArgumentException>(() => new ObjectPool<TestClass>(10, true));
+        }
+
+        [TestMethod()]
+        public void GetObjectTest()
+        {
+            int max = 10_000_000;
+            ObjectPool<TestClass> objectPool = new(max);
+            TestClass[] classes = new TestClass[max];
+
+            for (int i = 0; i < max; i++)
+            {
+                classes[i] = objectPool.GetObject();
+            }
+
+            TestClass c = objectPool.GetObject();
+
+            Random rand = new Random();
+            for (int i = 0; i < max; i++)
+            {
+                int n = rand.Next() % max;
+                classes[n].InUse = false;
+            }
+
+            for (int i = 0; i < max - 1; i++)
+            {
+                classes[i] = objectPool.GetObject();
+            }
+        }
+    }
+}
