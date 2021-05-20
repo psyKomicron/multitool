162d2b63fcb704d3ba8d7b1414856caab158a49b
psyKomicron <julien@cavaweb.net>
2021-05-20 22:20:51 +0200

    Create IPoolableObject.cs

---
+++ MultiTool_BL/Optimisation/IPoolableObject.cs
@@ -0,0 +1,15 @@
+﻿namespace Multitool.Optimisation
+{
+    public interface IPoolableObject
+    {
+        /// <summary>
+        /// Tells if the <see cref="IPoolableObject"/> is in use and thus cannot be used by the pool.
+        /// </summary>
+        bool InUse { get; set; }
+
+        /// <summary>
+        /// Fired when the object is free to be reused by the <see cref="ObjectPool{T}"/>.
+        /// </summary>
+        event FreeObjectEventHandler Free;
+    }
+}
