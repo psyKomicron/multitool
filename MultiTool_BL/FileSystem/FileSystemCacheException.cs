0b7079c90f856d5090b45ea0834e94b51c9eb071
psyKomicron <julien@cavaweb.net>
2021-05-20 23:10:13 +0200

    added new ctor with message and innerException

--- MultiTool_BL/FileSystem/FileSystemCacheException.cs
+++ MultiTool_BL/FileSystem/FileSystemCacheException.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Multitool.FileSystem
 {
     internal class FileSystemCacheException : Exception
     {
         public FileSystemCacheException(string message): base(message)
+        {
+        }
+
+        public FileSystemCacheException(string message, Exception innerException) : base(message, innerException)
         {
         }
     }
