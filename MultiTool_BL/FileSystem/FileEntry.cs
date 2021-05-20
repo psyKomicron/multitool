133e4279ce88316c322cc2b863fb6a2f92617934
psyKomicron <julien@cavaweb.net>
2021-05-20 23:09:44 +0200

    fixed typo in Size setter

--- MultiTool_BL/FileSystem/FileEntry.cs
+++ MultiTool_BL/FileSystem/FileEntry.cs
@@ -7,20 +7,21 @@ namespace Multitool.FileSystem
     {
         private readonly FileInfo _info;
 
+        public FileEntry(FileInfo info) : base(info)
+        {
+            _info = info;
+            FileInfo = info;
+        }
+
         public override long Size
         {
             get =>_info.Length;
             set
             {
-                throw new InvalidOperationException("Cannot set the size of a file, property is reliant on the actual file system infos.");
+                throw new InvalidOperationException("Cannot set the size of a file, property relies on the actual file system infos.");
             }
         }
 
-        protected override FileSystemInfo Info => _info;
-
-        public FileEntry(FileInfo info) : base(info)
-        {
-            _info = info;
-        }
+        public override FileSystemInfo Info => _info;
     }
 }
