73b4a4a3b0e8052ed7dcbcb6d0944b794d092793
Julien Marquet <49617466+psyKomicron@users.noreply.github.com>
2021-06-01 21:12:36 +0200

    added active attribute (and property) to properly display size status when using async methods

--- MultiTool_BL/NTInterop/PowerPlan.cs
+++ MultiTool_BL/NTInterop/PowerPlan.cs
@@ -4,16 +4,24 @@ namespace Multitool.NTInterop
 {
     public class PowerPlan
     {
-        private Guid guid;
-        private string name;
+        private Guid _guid;
+        private string _name;
+        private bool _active;
 
-        internal PowerPlan(Guid guid, string name)
+        internal PowerPlan(Guid guid, string name, bool active)
         {
-            this.guid = guid;
-            this.name = name;
+            _guid = guid;
+            _name = name;
+            _active = active;
         }
 
-        public Guid Guid => guid;
-        public string Name => name;
+        public Guid Guid => _guid;
+        public string Name => _name;
+        public bool Active => _active;
+
+        public override string ToString()
+        {
+            return "{Guid: " + Guid + ", Name " + Name + "}";
+        }
     }
 }
