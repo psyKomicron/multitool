f9db5fb2249e95d92fb5f1168c1e52489f1aab78
Julien Marquet <49617466+psyKomicron@users.noreply.github.com>
2021-06-01 21:02:54 +0200

    created usercontrol for the control panels

---
+++ Multitool/windows/ControlPanels/ControlPanelsWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace MultitoolWPF.Windows
+{
+    /// <summary>
+    /// Interaction logic for ControlPanelsWindow.xaml
+    /// </summary>
+    public partial class ControlPanelsWindow : Window
+    {
+        public ControlPanelsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MultitoolWindowChrome_CloseClick(object sender, RoutedEventArgs e) => Close();
+
+        private void MultitoolWindowChrome_MinimizeClick(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void MultitoolWindowChrome_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+
+            
+        }
+    }
+}
